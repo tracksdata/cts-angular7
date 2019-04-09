b436ba8a6b6e4224befa55bc7c799f80f7bdc08b
ctsjava <ctsgit@gmail.com>
2019-04-09 15:35:09 +0530

    Child Components

---
+++ UI/Angular/Product-APP/src/app/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  @Input()
+  cardHeader:string;
+  @Input()
+  productName:string;
+  @Input()
+  price:number;
+
+  ngOnInit() {
+  }
+
+}
